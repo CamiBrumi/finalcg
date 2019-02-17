a953c8972d96dd571ef9cf474ed5165aa4c00c86
Cami <cameliabrumar@gmail.com>
2019-02-17 00:19:28 -0500

    added a file with the work on normals

--- example.js
+++ example.js
@@ -9,6 +9,8 @@ var fovy = 50.0;  // Field-of-view in Y direction angle (in degrees)
 var aspect;       // Viewport aspect ratio
 var program;
 
+
+
 var mvMatrix, pMatrix;
 var modelView, projection;
 var eye;
@@ -28,10 +30,23 @@ var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
 var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
 var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
 
+var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
+var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0 );
+var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
+var materialShininess = 20.0;
+
 
 var xdir = {xpos:true, xneg:false};
 //var ydir = {ypos:true, yneg:false};
 var zSign = 1;
+
+var redCube = cube("r");
+var blueCube = cube("b");
+var greenCube = cube("g");
+
+var cubes = [cube("g"), cube("r"), cube("b")];
+
+var normalsArray = [];
 
 function main()
 {
@@ -62,6 +77,33 @@ function main()
     // Clear <canvas> by clearing the color buffer
     gl.enable(gl.DEPTH_TEST);
 
+
+    // set the lightning
+    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
+    var specularProduct = mult(lightSpecular, materialSpecular);
+    var ambientProduct = mult(lightAmbient, materialAmbient);
+
+
+    gl.uniform4fv(gl.getUniformLocation(program,
+        "diffuseProduct"), flatten(diffuseProduct));
+    gl.uniform4fv(gl.getUniformLocation(program,
+        "specularProduct"), flatten(specularProduct));
+    gl.uniform4fv(gl.getUniformLocation(program,
+        "ambientProduct"), flatten(ambientProduct));
+    gl.uniform4fv(gl.getUniformLocation(program,
+        "lightPosition"), flatten(lightPosition));
+    gl.uniform1f(gl.getUniformLocation(program,
+        "shininess"), materialShininess);
+
+    /*
+    var vBuffer2 = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer2);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+
+    var vNormal = gl.getAttribLocation( program, "vNormal");
+    gl.vertexAttribPointer(vNormal, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vNormal);*/
+
 	points = [];
 	colors = [];
 
@@ -83,10 +125,10 @@ function cube(color) //we define the faces here
     var verts = [];
     switch (color) {
         case "r":
-            verts = verts.concat(quad( 1, 0, 3, 2 ));
-            verts = verts.concat(quad( 2, 3, 7, 6 ));
-            verts = verts.concat(quad( 3, 0, 4, 7 ));
-            verts = verts.concat(quad( 6, 5, 1, 2 ));
+            verts = verts.concat(quad( 1, 0, 3, 2 )); //we add a face formed by 2 triangles
+            verts = verts.concat(quad( 2, 3, 7, 6 )); // we add another face
+            verts = verts.concat(quad( 3, 0, 4, 7 )); // in total we add 6 faces
+            verts = verts.concat(quad( 6, 5, 1, 2 )); // in total we add 12 triangles
             verts = verts.concat(quad( 4, 5, 6, 7 ));
             verts = verts.concat(quad( 5, 4, 0, 1 ));
             break;
@@ -115,15 +157,15 @@ function cube(color) //we define the faces here
     verts = verts.concat(quad( 6, 5, 1, 2 ));
     verts = verts.concat(quad( 4, 5, 6, 7 ));
     verts = verts.concat(quad( 5, 4, 0, 1 )); */
-    return verts;
+    return verts; // returns an array of 36 vertices
 }
 
 var id;
 function render()
 {
-    var redCube = cube("r");
+    /*var redCube = cube("r");
     var blueCube = cube("b");
-    var greenCube = cube("g");
+    var greenCube = cube("g");*/
     //var magentaCube = cube();
 
     pMatrix = perspective(fovy, aspect, .1, 1000);
@@ -163,7 +205,7 @@ function render()
     mvMatrix = mult(rotateY(theta), mvMatrix);
     gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
     //draw(blueCube, vec4(0.0, 0.0, 1.0, 1.0));
-    draw(greenCube, vec4(0.0, 1.0, 0.0, 1.0));
+    draw(cubes[0], vec4(0.0, 1.0, 0.0, 1.0));
     //draw(redCube, vec4(1.0, 0.0, 0.0, 1.0));
     //mvMatrix = stack.pop();
     //mvMatrix = stack.pop();
@@ -175,8 +217,8 @@ function render()
         mvMatrix = mult(translate(tx, 0, zSign*Math.sqrt(1-tx*tx)), mvMatrix); //translate(tx, 0, zSign*Math.sqrt(1-tx*tx))
         gl.uniformMatrix4fv( modelView, false, flatten(mvMatrix) );
         //draw(greenCube, vec4(0.0, 1.0, 0.0, 1.0));
-        draw(redCube, vec4(1.0, 0.0, 0.0, 1.0));
-        draw(blueCube, vec4(0.0, 0.0, 1.0, 1.0));
+        draw(cubes[1], vec4(1.0, 0.0, 0.0, 1.0));
+        draw(cubes[2], vec4(0.0, 0.0, 1.0, 1.0));
         //mvMatrix = stack.pop();
 /*
     stack.push(mvMatrix);
@@ -211,6 +253,8 @@ function draw(cube, color)
     {
         fragColors.push(color);
     }
+    console.log(cube.length);
+    console.log(fragColors.length);
 
     var pBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
@@ -219,6 +263,19 @@ function draw(cube, color)
     var vPosition = gl.getAttribLocation(program,  "vPosition");
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
+
+    //we compute the normals of the triangles
+    normalsArray = [];
+    for (var i = 0; i < cube.length/3; i++) {
+        newellMethod(cube[i*3], cube[i*3 + 1], cube[i*3 + 2]);
+    }
+    var vBuffer2 = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer2);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
+
+    var vNormal = gl.getAttribLocation( program, "vNormal");
+    gl.vertexAttribPointer(vNormal, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vNormal);
 
     var cBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
@@ -267,7 +324,7 @@ function quad(a, b, c, d)
 
     ];
 
-    var indices = [ a, b, c, a, c, d ];
+    var indices = [ a, b, c, a, c, d ]; // EACH FACE HAS GOT 6 VERTICES because it is composed by 2 triangles.
 
     for ( var i = 0; i < indices.length; ++i )
     {
@@ -277,64 +334,24 @@ function quad(a, b, c, d)
     return verts;
 }
 
-function newellMethod(a, b, c, d) {
+function newellMethod(a, b, c) {
     var nx = (a[1] - b[1]) * (a[2] + b[2]) + (b[1] - c[1]) * (b[2] + c[2]) + (c[1] - a[1]) * (c[2] + a[2]);
     var ny = (a[2] - b[2]) * (a[0] + b[0]) + (b[2] - c[2]) * (b[0] + c[0]) + (c[2] - a[2]) * (c[0] + a[0]);
     var nz = (a[0] - b[0]) * (a[1] + b[1]) + (b[0] - c[0]) * (b[1] + c[1]) + (c[0] - a[0]) * (c[1] + a[1]);
 
     var norm = Math.sqrt(nx*nx + ny*ny + nz*nz);
 
-    normals.push(vec3(nx/norm, ny/norm, nz/norm)); // normalized normal vectors
+    normalsArray.push(vec3(nx/norm, ny/norm, nz/norm)); // normalized normal vectors
 }
 
-window.onload = function init() {
+//computes every triangle's normal
+function triangleNormal(a, b, c) {
+    // normals are vectors
 
+    normalsArray.push(a[0],a[1], a[2], 0.0);
+    normalsArray.push(b[0],b[1], b[2], 0.0);
+    normalsArray.push(c[0],c[1], c[2], 0.0);
 
-    var diffuseProduct = mult(lightDiffuse, materialDiffuse);
-    var specularProduct = mult(lightSpecular, materialSpecular);
-    var ambientProduct = mult(lightAmbient, materialAmbient);
+    //index += 3;
 
-
-    gl.uniform4fv(gl.getUniformLocation(program,
-        "diffuseProduct"), flatten(diffuseProduct));
-    gl.uniform4fv(gl.getUniformLocation(program,
-        "specularProduct"), flatten(specularProduct));
-    gl.uniform4fv(gl.getUniformLocation(program,
-        "ambientProduct"), flatten(ambientProduct));
-    gl.uniform4fv(gl.getUniformLocation(program,
-        "lightPosition"), flatten(lightPosition));
-    gl.uniform1f(gl.getUniformLocation(program,
-        "shininess"), materialShininess);
-
-    /*
-    var vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW);
-
-    var vPosition = gl.getAttribLocation( program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vPosition);
-
-    var vBuffer2 = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer2);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(normalsArray), gl.STATIC_DRAW);
-
-    var vNormal = gl.getAttribLocation( program, "vNormal");
-    gl.vertexAttribPointer(vNormal, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(vNormal);
-
-    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
-    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
-
-    configureCubeMap();
-
-    var image = new Image();
-    image.crossOrigin = "";
-    image.src = "https://web.cs.wpi.edu/~jmcuneo/a.jpg";
-
-    image.onload = function() {
-        configureCubeMapImage(image);
-    }
-
-    render(); */
 }
